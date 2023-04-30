7049e9a675f8bccf402ded7de0bd1c66a86cfc38
chouettevan <pybots743@gmail.com>
2023-04-30 09:00:15 -0400

    Fixing category routes

--- src/Routes/Shop/Category-Page/category.jsx
+++ src/Routes/Shop/Category-Page/category.jsx
@@ -1,16 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../../../Contexts/product"; 
 import { useParams } from "react-router-dom";
 import Container from "../../../Components/Products/Container/container";
 import { CategoryTitle as Title } from "./styles";
 const CategoryPage  =  () => {
     const { category } = useParams();
-    const products  = useContext(ProductContext).products;
+    const productList  = useContext(ProductContext).products;
+    const [ products,setProducts ] = useState(productList[category]);
+    useEffect(() => {
+        setProducts(productList[category])
+    },[category,productList]);
     if (!products) return;
     return (
        <div>
             <Title>{category}</Title>
-            <Container products={products[category]}/>
+            <Container products={products}/>
         </div>
     );
 };
